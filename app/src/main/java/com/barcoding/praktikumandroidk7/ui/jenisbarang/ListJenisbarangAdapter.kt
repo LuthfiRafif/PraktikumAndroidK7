7cb0fb302737e80256fbed9c8b8ddf0fde61a53f
LuthfiRafif <luthfi0599@gmail.com>
2021-03-13 19:18:03 +0800

    web service menggunakan PHP yang akan berjalan secara local (Part4)

---
+++ app/src/main/java/com/barcoding/praktikumandroidk7/ui/jenisbarang/ListJenisbarangAdapter.kt
@@ -0,0 +1,29 @@
+package com.barcoding.praktikumandroidk7.ui.jenisbarang
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.barcoding.praktikumandroidk7.databinding.ItemRowJenisbarangBinding
+import com.barcoding.praktikumandroidk7.model.JenisbarangData
+
+class ListJenisbarangAdapter (private val listJenisbarang: ArrayList<JenisbarangData>) : RecyclerView.Adapter<ListJenisbarangAdapter.ListViewHolder>() {
+
+    class ListViewHolder(private val binding: ItemRowJenisbarangBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(jenisbarangData: JenisbarangData) {
+            with(binding){
+                tvNamajenisbarang.text = jenisbarangData.namajenisbarang
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemRowJenisbarangBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.bind(listJenisbarang[position])
+    }
+
+    override fun getItemCount():Int = listJenisbarang.size
+}
